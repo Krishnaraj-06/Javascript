553548a646cd17224925231f2054f8a2f45d740d
Krishnaraj-06 <165877909+Krishnaraj-06@users.noreply.github.com>
2025-06-02 18:23:58 +0000

    Day2

---
+++ .devcontainer/01_basics/variables.js
@@ -0,0 +1,9 @@
+const Name1="Krishnaraj"
+let namea="Krishna1"
+
+
+Name1="Pragya"
+namea="Krishnaaaa"
+
+/*Variables in JavaScript can be declared using `var`, `let`, or `const`, but we should use only const or let, var should not be used
+because it is not suitable for block scope */
