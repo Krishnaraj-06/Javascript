4ceed102f3124e41faf505f2b21c5ce8773a240f
Krishnaraj-06 <165877909+Krishnaraj-06@users.noreply.github.com>
2025-06-06 12:07:25 +0000

    Day2_variables

---
+++ .devcontainer/01_basics/datatypes.js
@@ -0,0 +1,18 @@
+"use strict"; // Treat all JS code as newer version
+
+//alert(3+3)//This code can work on browser but we are using NodeJS so it will give an error.
+
+
+// datatypes 
+Number
+BigInt
+String
+Boolean
+undefined//It is a type 
+null// It is a standalone value not a type
+
+//There is a differnce btwen Undefined and Null
+//for example
+let state;// Here state hasnt been assigned a value so its undefined
+//2nd Example
+let age=null//Here the age has been assigned a value which is null(not zero)
