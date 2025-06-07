1d3d7f1eeb8fe47f4145cbc24deabdf472f82886
Krishnaraj-06 <165877909+Krishnaraj-06@users.noreply.github.com>
2025-06-07 15:35:52 +0000

    Coversion_types

---
+++ .devcontainer/01_basics/conversion_operations.js
@@ -0,0 +1,19 @@
+// let score=false
+// console.log(typeof(score))
+// console.log(typeof score);
+
+// let valueinNumber=Number(score)
+// console.log(typeof valueinNumber);
+// console.log(valueinNumber);
+
+// //"33"=>33
+// //"33abc"=>NaN(Not a Number)
+// //true=> 1, false=>0
+
+
+//For Number to boolean
+let isLoggedIn=0
+let booleanValue=Boolean(isLoggedIn)
+
+console.log(booleanValue);
+//When string is empyt, boolean is false while if something is written then boolean value is 1
