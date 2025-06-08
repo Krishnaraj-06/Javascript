29f5cc7b41feef60b5d994e035e6f06e256d535a
Krishnaraj-06 <165877909+Krishnaraj-06@users.noreply.github.com>
2025-06-08 14:11:18 +0000

    Operations

--- .devcontainer/01_basics/conversion_operations.js
+++ .devcontainer/01_basics/conversion_operations.js
@@ -15,5 +15,38 @@
 let isLoggedIn=0
 let booleanValue=Boolean(isLoggedIn)
 
-console.log(booleanValue);
+// console.log(booleanValue);
 //When string is empyt, boolean is false while if something is written then boolean value is 1
+
+                     /*     Operations      */
+
+let a=10
+let b=20
+// console.log(a+b);
+// console.log(a-b);
+// console.log(a/b);
+// console.log(a*b);
+// console.log(a**b);
+// console.log(a%b);
+
+let str1="Krishna weds"
+let str2=" Pragya"
+// console.log(str1+str2);
+
+// console.log(1+"2");
+// console.log(2+"Krishna");
+// console.log(1+"2"+3);
+// console.log("1"+2);
+
+// console.log(2+3-4*3/2%1);
+
+let gc=100
+++gc
+console.log(gc);
+
+
+
+
+
+
+
