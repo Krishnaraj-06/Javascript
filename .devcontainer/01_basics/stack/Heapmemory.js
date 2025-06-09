c1b992a946d6289de01021f2f56e997512a4512c
Krishnaraj-06 <165877909+Krishnaraj-06@users.noreply.github.com>
2025-06-09 16:10:46 +0000

    MemoryInfo

---
+++ .devcontainer/01_basics/stack/Heapmemory.js
@@ -0,0 +1,11 @@
+//Stack Memory stores all Primitive datatypes in it.
+//Why?
+/*
+Primitives DT are super quick and light
+*/
+//Where as, Reference DT are stored in Heap Memory
+/*
+Objects are bigger, mutable and need large space,
+Reference is passed to other variable unlike copies in stack,
+any change in reference will change the original data too
+*/
