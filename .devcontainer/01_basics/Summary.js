f54e7e0a7d3619fe3ff7f8b5720f2bb0f333da9b
Krishnaraj-06 <165877909+Krishnaraj-06@users.noreply.github.com>
2025-06-09 15:15:03 +0000

    Summary-Day

---
+++ .devcontainer/01_basics/Summary.js
@@ -0,0 +1,22 @@
+//Types of datatypes
+//1. Primitive DT
+/*Number, BigInt, Boolean, String, null, undefined, symbol*/
+
+//2.Reference(Non-Primitive)DT
+/*Objects, Array, Functions*/
+
+
+//NOTE:
+//Symbol is used if for uniquness.
+//Example:
+let a=Symbol(123)
+let b=Symbol(123)
+console.log(a==b);
+
+
+const Hero=["Veer", "ChhotaBheem", "Nobita"]
+let myObj=
+{
+    name:Kabir,
+    age:10 
+}
