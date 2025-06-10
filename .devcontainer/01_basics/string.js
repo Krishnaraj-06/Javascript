6006bef8e00e4e9866b6f2db250d0e1f660c5e41
Krishnaraj-06 <165877909+Krishnaraj-06@users.noreply.github.com>
2025-06-10 12:26:49 +0000

    Strings

---
+++ .devcontainer/01_basics/string.js
@@ -0,0 +1,13 @@
+const name="Hello Ji Namaste"
+const age =19
+console.log(`${name} and my age is ${age}`);
+
+console.log(name.charAt(1));
+console.log(name.indexOf('a'));
+
+console.log(name.concat(" ", age));
+
+console.log(name.includes('Ji'));
+
+
+
