d8c3e7e725cfadb566316eea88fec4465d2248d1
Krishnaraj-06 <165877909+Krishnaraj-06@users.noreply.github.com>
2025-06-10 12:28:34 +0000

    again

--- .devcontainer/01_basics/string.js
+++ .devcontainer/01_basics/string.js
@@ -1,13 +1,20 @@
 const name="Hello Ji Namaste"
 const age =19
-console.log(`${name} and my age is ${age}`);
+// console.log(`${name} and my age is ${age}`);
 
-console.log(name.charAt(1));
-console.log(name.indexOf('a'));
+// console.log(name.charAt(1));
+// console.log(name.indexOf('a'));
 
-console.log(name.concat(" ", age));
+// console.log(name.concat(" ", age));
 
-console.log(name.includes('Ji'));
+// console.log(name.includes('Ji'));
 
+
+
+// console.log(name.toUpperCase());
+// console.log(name.toLowerCase());
+
+const str="    Krishna     "
+console.log(str.trim());
 
 
