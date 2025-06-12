e8aea226f38b7cb0cfb8834be0ea0ae2f5a2eb2c
Krishnaraj-06 <165877909+Krishnaraj-06@users.noreply.github.com>
2025-06-12 08:27:20 +0000

    Maths and Number

---
+++ .devcontainer/01_basics/Number.js
@@ -0,0 +1,34 @@
+const score=400
+// console.log(score);
+const num=new Number(100)
+// console.log(num);
+//In declaration of num, we have used the property of object by using new keyword, thus in printing output is different.
+//Here there are various methods, 
+//1) To convert the number to a string value:
+// console.log(num.toString());
+//2) To convert the huge number to a minimal decimal value:
+// console.log(num.toFixed(3));
+//3)To convert 10000n into better representation(by using commas):
+let number = 100000
+// console.log(number.toLocaleString('en-IN'));
+//4) Precision: Rounding off a value
+let a=123.092
+// console.log(a.toPrecision(2));
+
+
+
+//++++++++++++++++++++++++++++Maths++++++++++++++++++++++++//
+console.log(Math);
+//The Math Library in JS comes with various functions/Methods
+//1)abs(): It returns the positive value of any integer
+console.log(Math.abs(2));
+//2)round():It rounds of the decimal value
+console.log(Math.round(3.5));
+//3)ceil():GIF for a value, returns the top most value
+console.log(Math.ceil(3.2));
+//4)floor():LIF for a value, returns the bottom most value
+console.log(Math.floor(3.9));
+
+
+
+
