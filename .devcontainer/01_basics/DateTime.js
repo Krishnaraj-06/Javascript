a266405aa2f4a5c60758be0f41b3362eb78cd9d6
Krishnaraj-06 <165877909+Krishnaraj-06@users.noreply.github.com>
2025-06-15 18:55:49 +0000

    ToBeContinued

---
+++ .devcontainer/01_basics/DateTime.js
@@ -0,0 +1,13 @@
+//Date
+let myDate=new Date() 
+// console.log(myDate.toString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toJSON());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toLocaleString());
+// // console.log(myDate.toTimeString());
+
+
+
+
+
