11c068f6040bbb90d0c028d627068513f0f23d0b
Krishnaraj-06 <165877909+Krishnaraj-06@users.noreply.github.com>
2025-06-20 07:08:12 +0000

    ObjectDay1

---
+++ .devcontainer/02_basics/Objects.js
@@ -0,0 +1,16 @@
+/* What are Objects?
+Ans:In js, object is a core ds used to store structured data. It stores value in key-value pairs, and its dynamic, 
+allowing properties, to be added or removed. It is a reference type. Also, objects forms the backbone of JSON and are
+widely used in APIs and state management */
+
+//Objects can be declared in two ways:
+//Object Literals, Using Constructors 
+//Differnce kya hai? Simple constructor use karke singleton obj bana sakte whereas literal use krke multiple instance bana sakte
+
+
+//1) Object Literal:
+const User1={
+    name:"Krishna",
+     age:21,
+     isLogggedIn:true
+}
