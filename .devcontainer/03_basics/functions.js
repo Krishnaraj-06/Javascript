3458b690aeefc0502d7dece827214296f6a700af
Krishnaraj-06 <165877909+Krishnaraj-06@users.noreply.github.com>
2025-06-24 07:17:21 +0000

    aa

--- .devcontainer/03_basics/functions.js
+++ .devcontainer/03_basics/functions.js
@@ -1,7 +1,27 @@
 function sayName()
 {
-    console.log("I hate that bitch");
+    // console.log("I hate that bitch");
     
 }
 
 sayName() //Basic Definition of functions
+
+//Function to add two numbers:
+function addTwoNum(num1, num2) //Ye num1,num2 jo hai isko kehte parameters
+{
+    let result=num1+num2
+    return result
+}
+const result=addTwoNum(10,20)  //Ye 10,20 humne pass kiya usko kehte hai arguements
+// console.log("Result is", result); 
+
+
+
+//Another way to take a parameter
+function Hickey(Name1, Name2)
+{
+    return `${Name1} love ${Name2}`
+}
+console.log(Hickey("Krishnaraj", "Pragya"));
+//What if user ne koi value hi pass nahi kari?
+//So jo output me aata hai vo undefined show hota hai
